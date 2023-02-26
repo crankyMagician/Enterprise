143b2865a9387b7398beeed5993b9b0de1321378
Sam <74011113+crankyMagician@users.noreply.github.com>
2023-02-25 20:09:16 -0500

    JS Extensions implemented
    Added in toast javascript methods.

---
+++ Tangy/TangyWeb_Server/wwwroot/js/common.js
@@ -0,0 +1,11 @@
+﻿window.ShowToastr = (type, message) => {
+    console.log("JS Called");
+    if (type === "success") {
+        // Override global options
+        toastr.success(message, 'Operation succesful',  { timeOut: 5000 })
+    }
+    if (type === "error") {
+        // Override global options
+        toastr.error(message, 'Operation failed', { timeOut: 5000 })
+    }
+}
