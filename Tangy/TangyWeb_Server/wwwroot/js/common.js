1b3d0b70d74a21f822938abcfafc84ff9b11a092
Sam <74011113+crankyMagician@users.noreply.github.com>
2023-02-25 20:43:18 -0500

    Referenced child componenet
    referenced toaster method from child component

--- Tangy/TangyWeb_Server/wwwroot/js/common.js
+++ Tangy/TangyWeb_Server/wwwroot/js/common.js
@@ -9,3 +9,29 @@
         toastr.error(message, 'Operation failed', { timeOut: 5000 })
     }
 }
+
+
+window.ShowSwal = (type, message) => {
+    if (type === "success") {
+        Swal.fire(
+            'Success Notification!',
+            message,
+            'success'
+        )
+    }
+    if (type === "error") {
+        Swal.fire(
+            'Error Notification!',
+            message,
+            'error'
+        )
+    }
+}
+
+function ShowDeleteConfirmationModal() {
+    $('#deleteConfirmationModal').modal('show');
+}
+
+function HideDeleteConfirmationModal() {
+    $('#deleteConfirmationModal').modal('hide');
+}
